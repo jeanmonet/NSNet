10193fb2396b8b3db70b2a6d14e75f7ff23571b8
Jean Monet <jeanmonet@gmail.com>
2023-03-28 14:53:32 +0300

    lower batch size on SR (OOM)

--- scripts/sat_nsnet-sparrow_sr.sh
+++ scripts/sat_nsnet-sparrow_sr.sh
@@ -1,7 +1,7 @@
 # export MODEL_PATH="src/runs/nsnet_base/checkpoints/model_150.pt"
 
 # --- Infer marginals wiht NSNet and save in .out file for each CNF ---
-python src/run_model.py SATSolving/sr/test_hard --checkpoint $MODEL_PATH --model NSNet --n_rounds 10 --batch_size 32
+python src/run_model.py SATSolving/sr/test_hard --checkpoint $MODEL_PATH --model NSNet --n_rounds 10 --batch_size 16
 
 # --- Run Sparrow solver with initial marginal assignment from NSNet (above) ---
 python src/test_sat_solver.py SATSolving/sr/test_hard --solver Sparrow --max_flips 100 --n_process 8 --trial 0 --model NSNet
