51b0a668298c4bd85e76ea9a1adae59dab6ad96b
Jean Monet <jeanmonet@gmail.com>
2023-04-06 09:43:56 +0300

    Train CA sript. Make +x

--- scripts/sat_nsnet_ca.sh
+++ scripts/sat_nsnet_ca.sh
@@ -1,7 +1,14 @@
-python src/train_model.py sat-solving sat_nsnet_ca_marginal ~/scratch/NSNet/SATSolving/ca/train/ --valid_dir ~/scratch/NSNet/SATSolving/ca/valid/ --epochs 200 --scheduler ReduceLROnPlateau --lr_step_size 20 --loss marginal
-python src/test_model.py sat-solving ~/scratch/NSNet/SATSolving/ca/test/ --checkpoint runs/sat_nsnet_ca_marginal/checkpoints/model_best.pt
-python src/test_model.py sat-solving ~/scratch/NSNet/SATSolving/ca/test_hard/ --checkpoint runs/sat_nsnet_ca_marginal/checkpoints/model_best.pt
+python src/train_model.py sat-solving sat_nsnet_ca_marginal /opt/files/maio2022/SAT/NSNet/SATSolving/ca/train/ --valid_dir /opt/files/maio2022/SAT/NSNet/SATSolving/ca/valid/ --epochs 200 --scheduler ReduceLROnPlateau --lr_step_size 20 --loss marginal --batch_size 64 --save_model_epochs 5
 
-python src/train_model.py sat-solving sat_nsnet_ca_assignment ~/scratch/NSNet/SATSolving/ca/train/ --valid_dir ~/scratch/NSNet/SATSolving/ca/valid/ --epochs 200 --scheduler ReduceLROnPlateau --lr_step_size 20 --loss assignment
-python src/test_model.py sat-solving ~/scratch/NSNet/SATSolving/ca/test/ --checkpoint runs/sat_nsnet_ca_assignment/checkpoints/model_best.pt
-python src/test_model.py sat-solving ~/scratch/NSNet/SATSolving/ca/test_hard/ --checkpoint runs/sat_nsnet_ca_assignment/checkpoints/model_best.pt
+python src/test_model.py sat-solving /opt/files/maio2022/SAT/NSNet/SATSolving/ca/test/ --checkpoint runs/sat_nsnet_ca_marginal/checkpoints/model_best.pt --batch-size 64
+
+python src/test_model.py sat-solving /opt/files/maio2022/SAT/NSNet/SATSolving/ca/test_hard/ --checkpoint runs/sat_nsnet_ca_marginal/checkpoints/model_best.pt --batch-size 64
+
+
+# python src/train_model.py sat-solving sat_nsnet_ca_marginal ~/scratch/NSNet/SATSolving/ca/train/ --valid_dir ~/scratch/NSNet/SATSolving/ca/valid/ --epochs 200 --scheduler ReduceLROnPlateau --lr_step_size 20 --loss marginal
+# python src/test_model.py sat-solving ~/scratch/NSNet/SATSolving/ca/test/ --checkpoint runs/sat_nsnet_ca_marginal/checkpoints/model_best.pt
+# python src/test_model.py sat-solving ~/scratch/NSNet/SATSolving/ca/test_hard/ --checkpoint runs/sat_nsnet_ca_marginal/checkpoints/model_best.pt
+
+# python src/train_model.py sat-solving sat_nsnet_ca_assignment ~/scratch/NSNet/SATSolving/ca/train/ --valid_dir ~/scratch/NSNet/SATSolving/ca/valid/ --epochs 200 --scheduler ReduceLROnPlateau --lr_step_size 20 --loss assignment
+# python src/test_model.py sat-solving ~/scratch/NSNet/SATSolving/ca/test/ --checkpoint runs/sat_nsnet_ca_assignment/checkpoints/model_best.pt
+# python src/test_model.py sat-solving ~/scratch/NSNet/SATSolving/ca/test_hard/ --checkpoint runs/sat_nsnet_ca_assignment/checkpoints/model_best.pt
