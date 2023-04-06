2b46214e3b8855e4a9102e57fff850153a4a04ef
Jean Monet <jeanmonet@gmail.com>
2023-04-06 09:48:55 +0300

    Fix cli args for train scripts

--- scripts/sat_nsnet_ca.sh
+++ scripts/sat_nsnet_ca.sh
@@ -1,4 +1,4 @@
-python src/train_model.py sat-solving sat_nsnet_ca_marginal /opt/files/maio2022/SAT/NSNet/SATSolving/ca/train/ --valid_dir /opt/files/maio2022/SAT/NSNet/SATSolving/ca/valid/ --epochs 200 --scheduler ReduceLROnPlateau --lr_step_size 20 --loss marginal --batch_size 64 --save_model_epochs 5
+python src/train_model.py sat-solving --exp_id sat_nsnet_ca_marginal --train_dir /opt/files/maio2022/SAT/NSNet/SATSolving/ca/train/ --valid_dir /opt/files/maio2022/SAT/NSNet/SATSolving/ca/valid/ --epochs 200 --scheduler ReduceLROnPlateau --lr_step_size 20 --loss marginal --batch_size 64 --save_model_epochs 5
 
 python src/test_model.py sat-solving /opt/files/maio2022/SAT/NSNet/SATSolving/ca/test/ --checkpoint runs/sat_nsnet_ca_marginal/checkpoints/model_best.pt --batch-size 64
 
